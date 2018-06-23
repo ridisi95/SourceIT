d565a86e33eb4e1cb7ddee20f9b8e72c65d3c169
Oleksandr Smolin <smool-@ukr.net>
2018-06-23 14:14:28 +0300

    last fix on 23.06.18

--- src/HW13/MaxArrayValueMultithreading.java
+++ src/HW13/MaxArrayValueMultithreading.java
@@ -1,33 +1,35 @@
 package HW13;
 
-import java.util.Arrays;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class MaxArrayValueMultithreading {
 
-	public int[][] getRandomMatrix() {
+	private static int[][] mass = new int[4][4];
 
-		int[][] mass = new int[4][100000];
-		for (int i = 0; i < mass.length; i++) {
-			for (int j = 0; j < mass[1].length; j++) {
-				mass[i][j] = (int) (Math.random() * 100);
-			}
-		}
-		return mass;
+	private static int max;
+	private static int i;
+	private static int j;
+	private static AtomicBoolean stop = new AtomicBoolean(true);
+	private static ReentrantLock lock = new ReentrantLock();
+
+	private static ExecutorService executor = Executors.newSingleThreadExecutor();
+
+	private static void stopIt() {
+		stop.set(false);
 	}
 
-	public int getMaxValue(int mass[][]) {
-		int max = 0;
-		for (int i = 0; i < mass.length; i++) {
-			for (int j = 0; j < mass[1].length; j++) {
-				if (mass[i][j] > max || mass[i][j] == mass[0][1]) {
-					max = mass[i][j];
-				}
-			}
-		}
-		return max;
+	private static void createNewValue() {
+		mass[i][j] = (int) (Math.random() * 100);
+
 	}
 
-	public void printMatrix(int[][] mass) {
+	private static void printMatrix() {
 		for (int i = 0; i < mass.length; i++) {
 			for (int j = 0; j < mass[1].length; j++) {
 				System.out.print(mass[i][j] + ", ");
@@ -36,17 +38,68 @@ public class MaxArrayValueMultithreading {
 		}
 	}
 
-	public static void main(String[] args) {
-		/*
-		 * One thread spent ~ 4,01 seconds 
-		 */
-		
-		MaxArrayValueMultithreading obj = new MaxArrayValueMultithreading();
-		int[][] mass = obj.getRandomMatrix();
+	public static void main(String[] args) throws InterruptedException {
+
 		long startTime = System.nanoTime();
-		obj.printMatrix(mass);
-		System.out.println(obj.getMaxValue(mass));
-		long timeSpent = System.nanoTime() - startTime;
+		MatrixThread matrixThread1 = new MatrixThread();
+		Thread thread1 = new Thread(matrixThread1);
+
+		MatrixThread matrixThread2 = new MatrixThread();
+		Thread thread2 = new Thread(matrixThread2);
+
+		MatrixThread matrixThread3 = new MatrixThread();
+		Thread thread3 = new Thread(matrixThread3);
+
+		MatrixThread matrixThread4 = new MatrixThread();
+		Thread thread4 = new Thread(matrixThread4);
+
+		thread1.start();
+
+		thread1.join();
+
+		printMatrix();
+
+		System.out.println(max);
+		double timeSpent = (System.nanoTime() - startTime) / 1_000_000_000.0;
 		System.out.println(timeSpent);
+
 	}
-}
+
+	static class MatrixThread implements Runnable {
+
+		@Override
+		public void run() {
+			while (stop.get()) {
+				lock.lock();
+				if (stop.get()) {
+					if (j == mass[i].length) {
+						i++;
+						j = 0;
+					}
+					createNewValue();
+					j++;
+					if (i == mass.length - 1 && j == mass[i].length) {
+						stopIt();
+						j--;
+					}
+					lock.unlock();
+
+//					lock.lock();
+//					if (future.get() > max) {
+//						max = future.get();
+//					}
+//					lock.unlock();
+					
+
+						// try {
+						// Thread.sleep(445);
+						// } catch (InterruptedException e) {
+						// System.out.println(Thread.currentThread().getName() + " ended its work");
+						// }
+					}
+				}
+
+			}
+		}
+	}
+
