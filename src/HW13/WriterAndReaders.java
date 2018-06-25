cbee5c96ee988bb1c2d685ca5ff04617d264ce03
Oleksandr Smolin <smool-@ukr.net>
2018-06-25 12:07:18 +0300

    HW13 is Completed.

--- src/HW13/WriterAndReaders.java
+++ src/HW13/WriterAndReaders.java
@@ -1,124 +1,68 @@
 package HW13;
 
+import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class WriterAndReaders {
-
-	private static String str = "";
-	private static int index;
-	private static int regulator;
-	private static Boolean stop = true;
-	private static ReentrantLock lock = new ReentrantLock();
+	public static String str;
+	private static AtomicInteger regulator = new AtomicInteger();
 
 	private static class Writer implements Runnable {
-
-		String[] arr = { "FIRST line", "SECOND line", "THIRD line" };
-
-		// public WriterAndReaders_Writer(WriterAndReaders_Text str) {
-		// this.str = str;
-		// }
-
+		private int index;
+		private String[] massStr = { "FIRST STRING", "SECOND STRING", "THIRD STRING" };
 		@Override
 		public void run() {
 			while (true) {
-				if (index == 3) {
-					index = 0;
-				} else if (stop) {
-				try {
-					lock.lock();
-					str = arr[index];
-					System.out.println("Writer wrote: " + str);
-					Thread.currentThread().sleep(1000);
-				} catch (InterruptedException e) {
-					System.err.println("Writer is stoped");
+				if (regulator.get() == 4) {
+					regulator.set(0);
 				}
-				index++;
-				stop = false;
-				lock.unlock();
+				if (regulator.get() == 0) {
+					if (index == 3) {
+						break;
+					}
+					System.out.println("Wrote: " + massStr[index]);
+					str = massStr[index];
+					index++;
+					regulator.set(1);
 				}
 			}
 		}
-
-		// public String getStr() {
-		// return str;
-		// }
-		//
-		// public int getRegulator() {
-		// return regulator;
-		// }
-		//
-		// public void setRegulator() {
-		// this.regulator += 1;
-		// }
 	}
 
 	private static class Reader implements Runnable {
+		private Writer writer;
 		private String previousStr = "";
-
-		// public Reader(Writer writer) {
-		// this.writer = writer;
-		// }
-
+		public Reader(Writer writer) {
+			this.writer = writer;	
+		}
 		@Override
 		public void run() {
 			while (true) {
-				if (!previousStr.equals(str)) {
-					lock.lock();
-					previousStr = str;
+				if (regulator.get() >= 1 && !previousStr.equals(str)) {
 					System.out.println(Thread.currentThread().getName() + str);
-					try {
-						Thread.currentThread().sleep(1000);
-					} catch (InterruptedException e) {
-						System.err.println(Thread.currentThread().getName() + "is stoped");
-					}
-					regulator++;
-					if (regulator == 3) {
-						stop = true;
-					}
-					lock.unlock();
-				} else if(regulator == 3) {
-					regulator = 0;
-				}	
+					regulator.incrementAndGet();
+					previousStr = str;
+				}
 			}
-
 		}
 	}
 
-	public static void main(String[] args) {
-
+	public static void main(String[] args) throws InterruptedException {
 		Writer writer = new Writer();
 		Thread threadWriter = new Thread(writer);
-		Reader reader1 = new Reader();
-		Thread threadReader1 = new Thread(reader1);
-		threadReader1.setName("Reader-1 read:  ");
-		Reader reader2 = new Reader();
-		Thread threadReader2 = new Thread(reader2);
-		threadReader2.setName("Reader-2 read:  ");
-		Reader reader3 = new Reader();
-		Thread threadReader3 = new Thread(reader3);
-		threadReader3.setName("Reader-3 read:  ");
-
+		
+		ArrayList<Reader> listReaders = new ArrayList<>();
+		ArrayList<Thread> listThreads = new ArrayList<>(listReaders.size());
+		
 		threadWriter.start();
-		threadReader1.start();
-		threadReader2.start();
-		threadReader3.start();
-
-		try {
-			Thread.currentThread().sleep(6000);
-			System.out.println(threadWriter.getName() + threadWriter.isAlive());
-			System.out.println(threadReader1.getName() + threadWriter.isAlive());
-			System.out.println(threadReader2.getName() + threadWriter.isAlive());
-			System.out.println(threadReader3.getName() + threadWriter.isAlive());
-			// System.out.println(stop);
-			// System.out.println(regulator);
-			System.out.println("locked?  " + lock.isLocked());
-			// threadWriter.interrupt();
-			// threadReader1.interrupt();
-			// threadReader2.interrupt();
-			// threadReader3.interrupt();
-			// Thread.currentThread().interrupt();
-		} catch (InterruptedException e) {
+		for(int i = 0; i < 3; i++) {
+			listReaders.add(new Reader(writer));
+			listThreads.add(new Thread(listReaders.get(i)));
+			listThreads.get(i).setDaemon(true);
+			listThreads.get(i).setName("Reader-" + i + " read: ");
+			listThreads.get(i).start();
 		}
+		threadWriter.join();
 	}
-
 }
