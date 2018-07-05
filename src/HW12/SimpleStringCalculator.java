f960424e8445f3e40df519764e2e58ce723a672b
Oleksandr Smolin <smool-@ukr.net>
2018-07-05 08:31:01 +0300

    Completed Unit test except test with mocks.

--- src/HW12/SimpleStringCalculator.java
+++ src/HW12/SimpleStringCalculator.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class SimpleStringCalculator {
 
-	public int add(String numbers) {
+	public int add(String numbers) throws Exception {
 
 		int result = 0;
 
@@ -12,27 +12,24 @@ public class SimpleStringCalculator {
 			return 0;
 			
 		} else if (numbers.contains("-")) {
-			String[] substringMinus = numbers.replaceAll("(^\\d|[^-\\d]).+?(?=-|$)", "").replaceAll("\\D+", "--").split("\\b-");
-			
-			try {
-				throw new Exception("negatives not allowed");
-			} catch (Exception e) {
-				e.printStackTrace();
-				System.err.println(Arrays.toString(substringMinus));
-			}
-			
-		} else {
-			String[] substringPlus = numbers.concat("0").replaceAll("\\D", "").split("");
+			String[] substringMinus = numbers.replaceAll("(^\\d|[^-\\d]).+?(?=-|$)|\\D{2}", "").replaceAll("\\D+", "--")
+					.replaceFirst("^\\D", "").split("\\b-");
+			throw new Exception("negatives not allowed " + Arrays.toString(substringMinus));
+
+		} else if (numbers.matches("(.*)\\d(.*)")){
+			String[] substringPlus = numbers.replaceFirst("^.*?(?=\\d)", "").split("\\D+");
 			for (int i = 0; i < substringPlus.length; i++) {
+				if(Integer.parseInt(substringPlus[i]) <= 1000) {
 				result += Integer.parseInt(substringPlus[i]);
+				}
 			}
 		}
 		return result;
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		SimpleStringCalculator ssc = new SimpleStringCalculator();
-		String str = "-1238mc21u41uc123c1-123123-213141-2x12";
+		String str = "1001 1";
 		int x = ssc.add(str);
 		System.out.println(x);
 	}
--- src/HW12/TestSimpleStringCalculator.java
+++ src/HW12/TestSimpleStringCalculator.java
@@ -3,6 +3,7 @@ package HW12;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertSame;
 
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
@@ -11,29 +12,18 @@ import org.junit.rules.ExpectedException;
 
 public class TestSimpleStringCalculator{
 	
-	SimpleStringCalculator ssc = new SimpleStringCalculator();
-	String str = "כמצעמכצף1המהשעü2üהמüהü3";
-	int x;
-	
-	@Rule
-	  public ExpectedException thrown = ExpectedException.none();
-	
-	@BeforeClass
-	public static void beforeClass() {
-	}
-	
-	
 	@Test
-	public void testAdd() {
-	int x = ssc.add(str);
-	
-	assertEquals(x,6);
-	
+	public void testAdd() throws Exception {
+	assertEquals(new SimpleStringCalculator().add("כמצעמכצף1המהשעü2üהמüהü3"),6);
 	}
 	
 	@Test(expected = Exception.class)
-	  public void testExpectedException(){
-	    ssc.add("-12-123-4") ;
-//	    thrown.expect(java.lang.Exception.class);
-	  }
+	  public void testExpectedException() throws Exception{
+		new SimpleStringCalculator().add("12-12-34");
+	}
+	
+	@Test
+	public void testAddMoree1000() throws Exception {
+	assertEquals(new SimpleStringCalculator().add("1001 1"),1);
+	}
 }
